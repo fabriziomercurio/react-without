7dbd154b445d069e695a5e1616037127fb21dadf
fabriziomercurio <mercurio.fabrizio@gmail.com>
2025-11-24 15:46:57 +0100

    tanstack with pagination and dynamic data from api

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,16 +1,14 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react";
 import Navbar from "./Navbar"
+import { flexRender, getCoreRowModel, getPaginationRowModel, useReactTable, type ColumnDef } from "@tanstack/react-table";
 
-function Product()
-{ 
-  interface TypePost 
-  {
-    userId: number;
-    id: number;
-    name: string;
-    body: string;
-  }
-  const [data,setData] = useState<TypePost[]>([])
+function Product() {
+
+  const [dataset,setData] = useState<Product[]>([]); 
+  const [pagination, setPagination] = useState({
+  pageIndex: 0,
+  pageSize: 15, // numero di record per pagina
+  });
 
   useEffect(() => {
     fetch("http://localhost:8080/api/products")
@@ -18,23 +16,128 @@ function Product()
     .then((json) => { setData(json); console.log(json)})
   },[])
 
-  return(
+  interface Product {
+    id: number;
+    name: string;
+    price:number;
+    description:string;
+    available:number; 
+    brand:string; 
+    code:string;
+  }
+
+  const products = useMemo(() => dataset, [dataset]);
+
+  const columns: ColumnDef<Product>[] = [ 
+    {
+    accessorKey: 'id',
+    header: 'ID',
+    cell: info => info.getValue(),
+    },
+    {
+    accessorKey: 'name',
+    header: 'Name',
+    cell: info => info.getValue(),
+    },
+    {
+    accessorKey: 'price',
+    header: 'price',
+    cell: info => info.getValue(),
+    },
+    {
+    accessorKey: 'description',
+    header: 'description',
+    cell: info => {
+      const value = info.getValue() as string;
+      return value.length > 25 ? value.substring(0, 25) + "..." : value;
+      },
+     },
+    {
+    accessorKey: 'available',
+    header: 'available',
+    cell: info => info.getValue(),
+    },
+    {
+    accessorKey: 'brand',
+    header: 'brand',
+    cell: info => info.getValue(),
+    },
+    {
+    accessorKey: 'code',
+    header: 'code',
+    cell: info => info.getValue(),
+    },
+    {
+    id: 'actions',
+    header: 'Actions',
+    cell: (row => (
+      <div>
+         <button className="btn btn-danger m-1" onClick={() => alert(row.row.original.id)}>delete</button>
+         <button className="btn btn-warning m-1">edit</button>
+      </div>
+    ))
+    }
+  ]
+
+  const table = useReactTable<Product>({
+    data: products,
+    columns,
+    state: { pagination },
+    onPaginationChange: setPagination,
+    getCoreRowModel: getCoreRowModel(), 
+    getPaginationRowModel:getPaginationRowModel()
+  })
+
+  return (
     <>
-      <Navbar /> 
+      <Navbar />
       <div className="container mt-5">
-        <h2>List of products</h2> 
-         <div className="row">
-           {data.map((post:any) => ( 
-                <div key={post.id} className="mb-3">
-                  <h5>{post.name}</h5> 
-                  <p>{post.body}</p>
-                </div>
-              ))}
-         </div>
+        <table className="table table-striped">
+          <thead>
+            {
+              table.getHeaderGroups().map(headerGroup =>
+                <tr key={headerGroup.id}>
+                  {
+                    headerGroup.headers.map(header => (
+                      <th key={header.id}>
+                        {
+                          flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                      </th>
+                    ))}
+                </tr>
+              )}
+          </thead>
+          <tbody>
+            {
+              table.getRowModel().rows.map(row =>
+                <tr key={row.id}>
+                  {
+                    row.getVisibleCells().map(cell => (
+                      <td key={cell.id}>
+                        {
+                          flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                      </td>
+                    ))}
+                </tr>
+              )}
+          </tbody>
+        </table> 
+        <div className="mb-5 pagination">
+          <button className="border" onClick={() => table.setPageIndex(0)}>First Page</button>
+          <button className="border" disabled={!table.getCanPreviousPage()} onClick={() => table.previousPage()}>Previous Page</button>
+          <button className="border" disabled={!table.getCanNextPage()} onClick={() => table.nextPage()}>Next Page</button>
+          <button className="border" onClick={() => table.setPageIndex(table.getPageCount() - 1)}>Last Page</button>
+        </div> 
       </div>
     </>
   )
-} 
-export default Product 
+}
+export default Product
 
 
