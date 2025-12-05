3425461e0129de012c79e9cfeaa45ba4bff9d328
fabriziomercurio <mercurio.fabrizio@gmail.com>
2025-12-05 15:47:43 +0100

    add validation in Product form and add store method with fetch

--- src/components/NewProduct.tsx
+++ src/components/NewProduct.tsx
@@ -10,14 +10,14 @@ function NewProduct()
     messageError: string;
     data: {
         id: number;
-        name?: string;
-        price?: number;
-        category?: string;
-        description?: string;
-        available?: number;
-        brand?: string;
-        code?: string;
-        weight?: number;
+        name: string;
+        price: number;
+        category: string;
+        description: string;
+        available: number;
+        brand: string;
+        code: string;
+        weight: number;
     } | null;
     details:{
        status:boolean; 
@@ -28,30 +28,83 @@ function NewProduct()
     csrf: string;
     }
 
-   const [formData, setFormData] = useState<Partial<Omit<ProductResponse, 'data' | 'details'>> & {data?: Partial<ProductResponse['data']>; details?: Partial<ProductResponse['details']>}>({
-        success:true,
-        data: {
-            name: 'fabrizio', 
-            description:'testo di prova'
-        },
-        details: {
-            status: true
-        }
-    })
+   const [success, setSuccess] = useState(false); 
+   const [message, setMessage] = useState<string | boolean>(false); 
 
-    const handleSubmit = () => {
-        alert()
+   const [validation, setValidation] = useState<Record<string, string | number | boolean>>({})
+
+   const [formData, setFormData] = useState<Partial<Omit<ProductResponse, 'data' | 'details'>> & {data?: Partial<ProductResponse['data']>; details?: Partial<ProductResponse['details']>}>({
+       data:{
+        available:1 
+       }     
+   })
+
+   const handleInputChange = (e:any) => {
+      const {name, value} = e.target;      
+      setFormData({
+        ...formData,
+        data: { 
+            ...formData.data,
+            [name]: value,
+        }
+    }) 
     }
+
+    const handleSubmit = (e:any) => { 
+        e.preventDefault(); 
+        fetch('http://localhost:8080/api/product', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: formData.data?.name,
+            description: formData.data?.description
+        })
+        })
+        .then(response => {return response.json();} )
+        .then(result => {             
+             if (result.success) {
+               setMessage(result.message); 
+               setSuccess(true); 
+             }else{
+                setValidation(result); 
+             } 
+         })
+        .catch(error => { 
+           console.error('Errore nella richiesta:', error.message);
+        });
+    } 
+
     return(
         <> 
         <Navbar />
          <div className="container mt-5">
 
+            {success &&(
+            <div className="container">
+            <div className="alert alert-success alert-dismissible fade show" role="alert">
+                {message}
+                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+            </div>
+            </div>
+            )} 
+
             <form onSubmit={handleSubmit}> 
                 <label>Nome</label> 
-                <input className="form-control mb-3" type="text" name="name" value={formData.data?.name}/>
+                <input className={`form-control ${validation.name ? 'is-invalid' : ''} mb-3`} type="text" name="name" value={formData.data?.name} onChange={handleInputChange} />
+                 {validation.name && (
+                    <div className="invalid-feedback mb-3">
+                    {validation.name}
+                    </div>
+                 )}
                 <label>Description</label>
-                <textarea className="form-control mb-3" name="description" value={formData.data?.description}></textarea>
+                <textarea className={`form-control ${validation.description ? 'is-invalid' : ''} mb-3`} name="description" value={formData.data?.description} onChange={handleInputChange}></textarea>
+                {validation.description && (
+                    <div className="invalid-feedback mb-3">
+                    {validation.description}
+                    </div>
+                 )}
                 <button className="btn btn-outline-success">Send</button>
             </form>
          </div>
